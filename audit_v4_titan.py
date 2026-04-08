f41feb4a9070e847e93d17e046c28ff896250174
saharshasamudrala05 <146578971+saharshasamudrala05@users.noreply.github.com>
2026-04-08 21:26:11 +0530

    Delete audit_v4_titan.py

--- audit_v4_titan.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-conn = psycopg2.connect("postgresql://postgres:saharsha@localhost:5432/nexus_scm")
-cur = conn.cursor()
-cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'v4_titan_intelligence_fabric'")
-for row in cur.fetchall():
-    print(row[0])
-cur.close()
-conn.close()
